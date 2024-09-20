3b8cd1e7d306e520b474c659ccd5de48db3b189b
Shiva Yadav <shivayadavsy1256@gmail.com>
2024-09-20 09:57:59 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import  Dashboard  from "@/components/dashboard";
+
 import { SignInPageComponent } from "@/components/sign-in-page";
-import Link from "next/link";
 
 
 export default function Home() {
